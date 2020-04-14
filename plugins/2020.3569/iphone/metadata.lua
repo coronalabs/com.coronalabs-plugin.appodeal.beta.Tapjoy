74da036cbe821ff2c867ff6b4d7f40628dd063b5
Vlad Shcherban <vlad@coronalabs.com>
2020-04-14 00:05:21 -0400

    Adding binaries

---
+++ plugins/2020.3569/iphone/metadata.lua
@@ -0,0 +1,12 @@
+local metadata =
+{
+	plugin =
+	{
+		format = 'staticLibrary',
+		staticLibs = { 'APDTapjoyAdapter', },
+		frameworks = { 'Tapjoy', },
+		frameworksOptional = {},
+	},
+}
+
+return metadata
